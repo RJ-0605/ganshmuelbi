2bc246e351cf380115769bae0a2067365c9294a8
RJ-0605 <rojedkay@gmail.com>
2022-11-11 07:44:51 +0000

    test is able to check api health as well

--- Billing/tests/test_apis.py
+++ Billing/tests/test_apis.py
@@ -4,12 +4,31 @@ import pytest
 from .conftests import *
 
 def test_mock_route(client):
-    rv = client.get("/mock_route")
-    assert b"Hello World" == rv.data
+    rv = client.get("/mock_routeK")
+    
 
-    print("##### Started Testing the apis status code######")
-    assert rv.status_code is 200 
+    print("##### Begin Testing the apis status code######")
+
+    assert rv.status_code is 200 , f" 200 status_code  if successful expected, got something else"
+    
+
     print("##### Ended Testing the apis status code######")
+    print("#################################################")
+    print("#################################################")
+
+
+
+def test_mock_route_return_value(client):
+    rv = client.get("/mock_route")
+
+
+    print("##### Started Testing the return  ######")
+
+    assert b"Hello World" == rv.data, f" 'Hello World' return statement  if successful expected, got something else"
+
+    print("##### Ended Testing the apis status code######")
+
+    
 
 
 
